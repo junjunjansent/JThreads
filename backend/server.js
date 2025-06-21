245a38e12be3c5b6172818d1f5d4f0f4620437c9
Jacob Tham <jacob@mac.lan>
2025-06-21 13:40:27 +0800

    jacob: server setup

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,43 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const logger = require("morgan");
+const dotenv = require("dotenv");
+dotenv.config();
+
+// Import routers
+// const authRouter = require("./controllers/auth");
+// const testJwtRouter = require("./controllers/test-jwt");
+// const usersRouter = require("./controllers/users");
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.set("debug", true);
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(logger("dev"));
+
+// Routes
+// app.use("/auth", authRouter);
+// app.use("/test-jwt", testJwtRouter);
+// app.use("/users", usersRouter);
+
+// Start the server and listen on port 3000
+app.listen(3000, () => {
+  console.log("The express app is ready!");
+});
+
 // import express from "express";
 // // const express = require("express");
 // import publicRoutes from "./routes/publicRoutes.js";
 // import userRoutes from "./routes/userRoutes";
 // import { isSignedIn } from "./middleware/authenticator";
-
-// // config dotenv
-// import dotenv from "dotenv";
-// dotenv.config();
 
 // // config mongoose
 // import mongoose from "mongoose";
