6e1f7caaf920ab583128f5dffbe8d29b40d3826e
Jacob Tham <jacob@mac.lan>
2025-06-21 17:00:39 +0800

    jacob: registration

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,8 +1,56 @@
+// Creating a sign up function
+
+const signUp = async (data) => {
+  const url = `${import.meta.env.VITE_BACK_END_SERVER_URL}/sign-up`;
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
 const RegisterPage = () => {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+
+    await signUp(data);
+  };
   return (
-    <>
-      <h1>JThreads</h1>
-    </>
+    <form onSubmit={handleSubmit}>
+      <fieldset>
+        <legend>Sign Up</legend>
+
+        <label>
+          Username <input name="username" />
+        </label>
+        <br />
+        <label>
+          Email <input name="email" type="text" />
+        </label>
+        <br />
+        <label>
+          Password <input name="password" type="text" />
+        </label>
+        <br />
+
+        <button>Sign Up</button>
+      </fieldset>
+    </form>
   );
 };
 
