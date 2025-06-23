6669da79d68e24f7dd5698ff240a17c7e8faa159
Jacob Tham <jacob@mac.lan>
2025-06-23 20:36:58 +0800

    jacob: product card edits

--- frontend/src/components/ProductCard/ProductCard.jsx
+++ frontend/src/components/ProductCard/ProductCard.jsx
@@ -8,55 +8,14 @@ const ProductCard = () => {
           src="https://homeapparel254.com/cdn/shop/collections/Daark-Brown-Tshirt-254-White.png?v=1662469719&width=2048"
         />
         <div className={styles.productName}>Teal T-shirt</div>
-        <div className={styles.productPrice}>S$ 15.90</div>
-      </div>
-      <div className={styles.productCard}>
-        <img
-          className={styles.productImage}
-          src="https://www.wetteeshirt.co/wp-content/uploads/tshirt-mockup-of-a-man-frowing.jpg"
-        />
-        <div className={styles.productName}>Teal T-shirt</div>
-        <div className={styles.productPrice}>S$ 15.90</div>
-      </div>
-      <div className={styles.productCard}>
-        <img
-          className={styles.productImage}
-          src="https://homeapparel254.com/cdn/shop/collections/Daark-Brown-Tshirt-254-White.png?v=1662469719&width=2048"
-        />
-        <div className={styles.productName}>Teal T-shirt</div>
-        <div className={styles.productPrice}>S$ 15.90</div>
-      </div>
-      <div className={styles.productCard}>
-        <img
-          className={styles.productImage}
-          src="https://homeapparel254.com/cdn/shop/collections/Daark-Brown-Tshirt-254-White.png?v=1662469719&width=2048"
-        />
-        <div className={styles.productName}>Teal T-shirt</div>
-        <div className={styles.productPrice}>S$ 15.90</div>
-      </div>
-      <div className={styles.productCard}>
-        <img
-          className={styles.productImage}
-          src="https://homeapparel254.com/cdn/shop/collections/Daark-Brown-Tshirt-254-White.png?v=1662469719&width=2048"
-        />
-        <div className={styles.productName}>Teal T-shirt</div>
-        <div className={styles.productPrice}>S$ 15.90</div>
-      </div>
-      <div className={styles.productCard}>
-        <img
-          className={styles.productImage}
-          src="https://homeapparel254.com/cdn/shop/collections/Daark-Brown-Tshirt-254-White.png?v=1662469719&width=2048"
-        />
-        <div className={styles.productName}>Teal T-shirt</div>
-        <div className={styles.productPrice}>S$ 15.90</div>
-      </div>
-      <div className={styles.productCard}>
-        <img
-          className={styles.productImage}
-          src="https://homeapparel254.com/cdn/shop/collections/Daark-Brown-Tshirt-254-White.png?v=1662469719&width=2048"
-        />
-        <div className={styles.productName}>Teal T-shirt</div>
-        <div className={styles.productPrice}>S$ 15.90</div>
+        <div className={styles.productSubContainer}>
+          <div className={styles.productCategory}>Hats</div>
+          <div className={styles.productSeller}>Seller: jt123</div>
+        </div>
+        <div className={styles.productSubContainer}>
+          <div className={styles.productPriceRange}>S$ 15.90</div>
+          <div className={styles.productQuantity}>15 avail</div>
+        </div>
       </div>
     </>
   );
