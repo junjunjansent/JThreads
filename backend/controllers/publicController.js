e2e5811c3da7877d839ad1be26a7439b378924c6
Jansen <57595871+junjunjansent@users.noreply.github.com>
2025-06-24 14:24:20 +0800

    Jansen: Error Handler & Route Cleaning

--- backend/controllers/publicController.js
+++ backend/controllers/publicController.js
@@ -1,3 +1,59 @@
+const User = require("../models/User");
+const { ApiError } = require("../utils/errorHandler");
+const { bcryptPassword } = require("../utils/bcrypt");
+const { createJWT } = require("../utils/jwt");
+
+const signUp = async (req, res, next) => {
+  try {
+    const { username, password, email } = req.body;
+    const userExisting = await User.findOne({ username: username });
+
+    if (userExisting) {
+      throw new ApiError({
+        status: {
+          status: "409",
+          source: { pointer: "publicController.js" },
+          title: "User Exists",
+          detail: "Username already taken.",
+        },
+      });
+    }
+
+    const newUser = await User.create({
+      username: username,
+      password: bcryptPassword(password),
+      email: email,
+      // JANSEN COMMENTS - all these should be optional first
+      //   firstname: req.body.firstname,
+      //   lastname: req.body.lastname,
+      //   birthday: req.body.age,
+
+      //   gender: req.body.gender,
+      //   phoneNumber: req.body.phoneNumber, // not sure why this is not being captured in mongo
+    });
+
+    if (!newUser) {
+      throw new ApiError({
+        status: {
+          status: "400",
+          source: { pointer: "publicController.js" },
+          title: "Invalid User",
+          detail: "User could not be created.",
+        },
+      });
+    }
+
+    const payload = { username: newUser.username, _id: newUser._id };
+    const token = createJWT(payload);
+
+    res.status(201).json({ data: { token } });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { signUp };
+
 // // import { Recipe } from "../models/Recipe";
 // import { User } from "../models/User";
 
