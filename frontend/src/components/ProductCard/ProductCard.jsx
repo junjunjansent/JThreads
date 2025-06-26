bd4d11a716aa9288b52dd1c5e8c0f911f167414f
Jacob Tham <jacob@Jacobs-MacBook-Pro-2.local>
2025-06-26 13:46:43 +0800

    jacob: public buy page displays all products

--- frontend/src/components/ProductCard/ProductCard.jsx
+++ frontend/src/components/ProductCard/ProductCard.jsx
@@ -1,23 +1,19 @@
 import styles from "./ProductCard.module.css";
-const ProductCard = ({ allProducts }) => {
+
+const ProductCard = ({ key, name, category, photo, owner, isActive }) => {
   return (
-    <>
-      <div className={styles.productCard}>
-        <img
-          className={styles.productImage}
-          src="https://homeapparel254.com/cdn/shop/collections/Daark-Brown-Tshirt-254-White.png?v=1662469719&width=2048"
-        />
-        <div className={styles.productName}>Teal T-shirt</div>
-        <div className={styles.productSubContainer}>
-          <div className={styles.productCategory}>Hats</div>
-          <div className={styles.productSeller}>Seller: jt123</div>
-        </div>
-        <div className={styles.productSubContainer}>
-          <div className={styles.productPriceRange}>S$ 15.90</div>
-          <div className={styles.productQuantity}>15 avail</div>
-        </div>
+    <div className={styles.productCard}>
+      <img className={styles.productImage} src={photo} />
+      <div className={styles.productName}>{name}</div>
+      <div className={styles.productSubContainer}>
+        <div className={styles.productCategory}>{category}</div>
+        <div className={styles.productSeller}>Seller: {owner}</div>
       </div>
-    </>
+      <div className={styles.productSubContainer}>
+        <div className={styles.productPriceRange}>S$ 15.90</div>
+        <div className={styles.productQuantity}>15 avail</div>
+      </div>
+    </div>
   );
 };
 
