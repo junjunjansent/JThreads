9a57fc4792968045cf81f5fe6d1ab5b2454c1060
Jansen <57595871+junjunjansent@users.noreply.github.com>
2025-06-29 18:17:49 +0800

    Jansen: Basic Cart In-progress

--- frontend/src/pages/user/buyer/BuyerCartPage.jsx
+++ frontend/src/pages/user/buyer/BuyerCartPage.jsx
@@ -1,7 +1,49 @@
+import { useEffect, useState } from "react";
+import { PageStatusTypes } from "../../../utils/pageStatusUtil";
+import Loader from "../../../components/Loader";
+import ErrorPage from "../../ErrorPage";
+
+import { showOwnerCart } from "../../../services/cartServices";
+import { errorUtil } from "../../../utils/errorUtil";
+
 const BuyCartPage = () => {
+  const [cart, setCart] = useState(null);
+  const [pageStatus, setPageStatus] = useState(PageStatusTypes.LOADING);
+
+  useEffect(() => {
+    const getCart = async () => {
+      const controller = new AbortController();
+      try {
+        setPageStatus(PageStatusTypes.LOADING);
+        const { cart } = await showOwnerCart();
+        setCart(cart);
+        setPageStatus(PageStatusTypes.OK);
+      } catch (err) {
+        setPageStatus(PageStatusTypes.ERROR);
+        errorUtil(err);
+      }
+
+      return () => {
+        // Cleanup on unmount
+        controller.abort();
+      };
+    };
+
+    getCart();
+  }, []);
+
+  switch (pageStatus) {
+    case PageStatusTypes.LOADING:
+      return <Loader />;
+    case PageStatusTypes.ERROR:
+      return <ErrorPage />;
+    default:
+      break;
+  }
   return (
     <>
       <h1>BuyCartPage</h1>
+      <pre>{JSON.stringify(cart, null, 2)}</pre>
     </>
   );
 };
---
+++ frontend/src/services/cartServices.js
@@ -0,0 +1,63 @@
+import { ApiError } from "../utils/errorUtil";
+import { getTokenFromLocalStorage } from "../utils/tokenUtil";
+
+const cartService_BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/cart`;
+
+const cartService_HEADER = () => {
+  const token = getTokenFromLocalStorage();
+  if (token) {
+    return {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    };
+  } else {
+    return {
+      "Content-Type": "application/json",
+    };
+  }
+};
+
+const fetchJson = async (url, methodStr = "GET", bodyData = null) => {
+  const options = {
+    method: methodStr,
+    headers: cartService_HEADER(),
+  };
+
+  if (methodStr === "POST" || methodStr === "PUT") {
+    if (bodyData) {
+      options.body = JSON.stringify(bodyData);
+    } else {
+      throw new ApiError({
+        status: 405,
+        source: { pointer: "cartServices.js" },
+        title: "Method Not Allowed",
+        detail: "Need Body Data for this method.",
+      });
+    }
+  }
+
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw ApiError.fromFetchResponses(data);
+  }
+
+  return data;
+};
+
+// ----------- actual Services
+
+const showOwnerCart = async () => {
+  // private Router should have protected any unauthorised getting in
+  const url = `${cartService_BASE_URL}`;
+
+  try {
+    const resData = await fetchJson(url, "GET");
+    return resData;
+  } catch (err) {
+    throw new ApiError(err);
+  }
+};
+
+export { showOwnerCart };
