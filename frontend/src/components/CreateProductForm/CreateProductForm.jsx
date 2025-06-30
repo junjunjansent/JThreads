fbd1c6e88a862067039a3e5400493b1f1b84e9bb
Jacob <jacobtham3@gmail.com>
2025-06-30 15:44:58 +0800

    jacob: controlled form for create product done

--- frontend/src/components/CreateProductForm/CreateProductForm.jsx
+++ frontend/src/components/CreateProductForm/CreateProductForm.jsx
@@ -1,4 +1,23 @@
+import { useState } from "react";
+
 const CreateProductForm = ({ isDialogOpen, toggleDialog }) => {
+  const [, setCreateProductDetails] = useState({
+    productName: "",
+    productCategory: "Tops",
+    productDescription: "",
+    productDisplayPhoto: "",
+    productDefaultDeliveryTime: "",
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    // Jacob: prev details needed here to ensure existing changes are not overriden
+    setCreateProductDetails((prevDetails) => ({
+      ...prevDetails,
+      [name]: value,
+    }));
+  };
+
   if (!isDialogOpen) {
     return null;
   }
@@ -6,30 +25,32 @@ const CreateProductForm = ({ isDialogOpen, toggleDialog }) => {
     <>
       <dialog open>
         Create a new product!
-        <input name="productName" type="text" placeholder="Product Name" />
-        <select name="productCategory">
-          <option value="Tops">Tops</option>
-          <option value="Bottoms">Bottoms</option>
-          <option value="Headwear">Headwear</option>
-          <option value="Bags">Bags</option>
-          <option value="Accesories">Accessories</option>
-          <option value="Misc">Misc</option>
-        </select>
-        <input
-          name="productDescription"
-          type="text"
-          placeholder="Product Description"
-        />
-        <input
-          name="productDisplayPhoto"
-          type="text"
-          placeholder="Product Image"
-        />
-        <input
-          name="productDefaultDeliveryTime"
-          type="text"
-          placeholder="Delivery Time"
-        />
+        <form onChange={handleInputChange}>
+          <input name="productName" type="text" placeholder="Product Name" />
+          <select name="productCategory">
+            <option value="Tops">Tops</option>
+            <option value="Bottoms">Bottoms</option>
+            <option value="Headwear">Headwear</option>
+            <option value="Bags">Bags</option>
+            <option value="Accesories">Accessories</option>
+            <option value="Misc">Misc</option>
+          </select>
+          <input
+            name="productDescription"
+            type="text"
+            placeholder="Product Description"
+          />
+          <input
+            name="productDisplayPhoto"
+            type="text"
+            placeholder="Product Image"
+          />
+          <input
+            name="productDefaultDeliveryTime"
+            type="text"
+            placeholder="Delivery Time"
+          />
+        </form>
         <button onClick={toggleDialog}>Close Dialog</button>
         <button>Create Product</button>
       </dialog>
