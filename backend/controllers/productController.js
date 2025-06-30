ee9c3670f398819c6ee4ddbb450a80ce45a241f6
Jacob <jacobtham3@gmail.com>
2025-06-30 18:58:44 +0800

    jacob: put backend done

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,11 +1,11 @@
 const Product = require("../models/Product");
-const { getUserFromRequest } = require("../utils/tokenHandler");
+// const { getUserFromRequest } = require("../utils/tokenHandler");
 
 // SELLER CREATING NEW PRODUCT
 const createOne = async (req, res, next) => {
   try {
-    const user = getUserFromRequest(req);
-    req.body.productOwner = user._id;
+    // const user = getUserFromRequest(req);
+    // req.body.productOwner = user._id;
     const createProduct = await Product.create(req.body);
     res.status(201).json({ product: createProduct });
   } catch (err) {
@@ -13,4 +13,17 @@ const createOne = async (req, res, next) => {
   }
 };
 
-module.exports = { createOne };
+const editOne = async (req, res, next) => {
+  try {
+    const productId = req.params.productId;
+    const productEdit = req.body;
+    // const createProduct = await Product.findByIdAndUpdate(productId, req.body);
+    const editProduct = await Product.findByIdAndUpdate(productId, productEdit);
+    const updatedProduct = await Product.findById(productId);
+    res.status(201).json(updatedProduct);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { createOne, editOne };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { createOne } = require("../controllers/productController");
+const { createOne, editOne } = require("../controllers/productController");
 
 // specific routes
 router.post("/", createOne);
+router.put("/:productId", editOne);
 
 module.exports = router;
