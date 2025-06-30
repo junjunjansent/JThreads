d21ab2403f1c89cf4901c55cbad0a7c04136b74d
Jacob <jacobtham3@gmail.com>
2025-06-30 15:24:21 +0800

    jacob: basic create product form done

--- frontend/src/components/CreateProductForm/CreateProductForm.jsx
+++ frontend/src/components/CreateProductForm/CreateProductForm.jsx
@@ -4,9 +4,32 @@ const CreateProductForm = ({ isDialogOpen, toggleDialog }) => {
   }
   return (
     <>
-      <p>Dialog Open</p>
       <dialog open>
-        Test
+        Create a new product!
+        <input name="productName" type="text" placeholder="Product Name" />
+        <select name="productCategory">
+          <option value="Tops">Tops</option>
+          <option value="Bottoms">Bottoms</option>
+          <option value="Headwear">Headwear</option>
+          <option value="Bags">Bags</option>
+          <option value="Accesories">Accessories</option>
+          <option value="Misc">Misc</option>
+        </select>
+        <input
+          name="productDescription"
+          type="text"
+          placeholder="Product Description"
+        />
+        <input
+          name="productDisplayPhoto"
+          type="text"
+          placeholder="Product Image"
+        />
+        <input
+          name="productDefaultDeliveryTime"
+          type="text"
+          placeholder="Delivery Time"
+        />
         <button onClick={toggleDialog}>Close Dialog</button>
         <button>Create Product</button>
       </dialog>
