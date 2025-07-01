8fed4565bf4d1c7c333685baae93da243d2f28f5
Jansen <57595871+junjunjansent@users.noreply.github.com>
2025-07-01 13:31:35 +0800

    Jansen: deleteCart Done, updateCart in progress

--- backend/utils/inputValidator.js
+++ backend/utils/inputValidator.js
@@ -3,17 +3,20 @@
 const { ApiError } = require("./errorHandler");
 
 const usernameValidator = (username) => {
-  if (
-    username.length < 3 ||
-    username.length > 20 ||
-    !/^[a-z0-9_-]+$/.test(username)
-  ) {
+  if (username.length < 3 || username.length > 20) {
+    throw new ApiError({
+      status: 400,
+      source: { pointer: "inputValidator.js" },
+      title: "Bad Request: Username Format",
+      detail: "Username must be 3-20 characters long.",
+    });
+  } else if (!/^[a-z0-9_-]+$/.test(username)) {
     throw new ApiError({
       status: 400,
       source: { pointer: "inputValidator.js" },
       title: "Bad Request: Username Format",
       detail:
-        "Username must be 3-20 characters long, and can only contain lowercase letters, numbers, hyphens, and underscores.",
+        "Username must only contain lowercase letters, numbers, hyphens, and underscores.",
     });
   }
 };
@@ -67,7 +70,19 @@ const phoneNumberValidator = (phoneNumber) => {
   }
 };
 
-const numberRangeValidator = (number, min = 0, max = Infinity) => {
+/**
+ * Checks if number is in range and type given
+ *
+ * @param {number|string} number - Value to check
+ * @param {Object} [options={}] - option (See below)
+ * @param {number} [options.min=0]
+ * @param {number} [options.max=Infinity]
+ * @param {"integer"|"price"} [options.type="integer"] - Type of number to validate.
+ */
+const numberRangeValidator = (
+  number,
+  { min = 0, max = Infinity, type = "integer" } = {}
+) => {
   const convertedNum = Number(number);
   if (isNaN(convertedNum) || convertedNum < min || convertedNum > max) {
     throw new ApiError({
@@ -75,6 +90,24 @@ const numberRangeValidator = (number, min = 0, max = Infinity) => {
       source: { pointer: "inputValidator.js" },
       title: "Bad Request: Number Range Format",
       detail: `Number must be a valid number within ${min} and ${max}.`,
+    });
+  }
+  if (type === "integer" && !Number.isInteger(convertedNum)) {
+    throw new ApiError({
+      status: 400,
+      source: { pointer: "inputValidator.js" },
+      title: "Bad Request: Number Type",
+      detail: `Number is not the same type as indicated: expected ${type}.`,
+    });
+  } else if (
+    type === "price" &&
+    (convertedNum < 0 || !/^\d+(\.\d{1,2})?$/.test(convertedNum.toString()))
+  ) {
+    throw new ApiError({
+      status: 400,
+      source: { pointer: "inputValidator.js" },
+      title: "Bad Request: Number Type",
+      detail: `Number is not the same type as indicated: expected ${type}.`,
     });
   }
 };
